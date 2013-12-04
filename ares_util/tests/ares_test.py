072d54f589b88ba2b24e8eaea429d8e9979ba934
illagrenan <vaclav.dohnal@gmail.com>
2013-12-04 17:16:11 +0100

    Added test for company without orientation number in registered address (CTU)

--- ares_util/tests/ares_test.py
+++ ares_util/tests/ares_test.py
@@ -15,11 +15,16 @@ class CallARESTestCase(TestCase):
         self.assertFalse(call_ares("foo"))
 
     def test_valid_values(self):
-        self.assertIsInstance(call_ares(27074358), dict)
+        self.assertIsInstance(call_ares(company_id=27074358), dict)
 
         # IČ 25596641 je numericky platné, není však zaregistrované
         # Viz http://phpfashion.com/jak-overit-platne-ic-a-rodne-cislo
-        self.assertFalse(call_ares(25596641), dict)
+        self.assertFalse(call_ares(company_id=25596641), dict)
+
+        actual = call_ares(company_id=68407700)['address']['street']
+        # ČVUT v Praze
+        expected = "Zikova 1903"
+        self.assertEqual(actual, expected)
 
 
 if __name__ == '__main__':
