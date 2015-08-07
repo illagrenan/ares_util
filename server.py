2b604c8c2c5443e69606deedb7d8edebad1d5e48
illagrenan <vaclav.dohnal@gmail.com>
2015-08-07 12:09:20 +0200

    Fixed JSON serialization (for non-dict data) in Flask.

--- server.py
+++ server.py
@@ -5,6 +5,8 @@ from __future__ import unicode_literals
 from __future__ import print_function
 from __future__ import absolute_import
 
+import json
+
 from flask import Flask
 import flask
 
@@ -13,11 +15,12 @@ from ares_util.ares import call_ares
 app = Flask(__name__)
 
 
-@app.route('/<string:post_id>')
+@app.route('/<string:post_id>', methods=['GET', 'OPTIONS'])
 def ares_json_response(post_id):
     ares_response = call_ares(company_id=post_id)
+    json_data = json.dumps(ares_response)
 
-    return flask.jsonify(**ares_response)
+    return flask.Response(json_data, status=200, mimetype='application/json')
 
 
 if __name__ == '__main__':
