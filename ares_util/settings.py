33ae61bd148cfdc1f138dda1421a7fcd4c9a0481
illagrenan <vaclav.dohnal@gmail.com>
2016-03-02 15:59:30 +0100

    Compact future imports.

--- ares_util/settings.py
+++ ares_util/settings.py
@@ -1,10 +1,7 @@
 # !/usr/bin/python
 # coding=utf-8
 
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-from __future__ import unicode_literals
+from __future__ import (absolute_import, division, print_function, unicode_literals)
 
 COMPANY_ID_LENGTH = 8
 ARES_API_URL = 'http://wwwinfo.mfcr.cz/cgi-bin/ares/darv_bas.cgi'
--- ares_util/tests/ares_test.py
+++ ares_util/tests/ares_test.py
@@ -1,10 +1,7 @@
 # !/usr/bin/python
 # coding=utf-8
 
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-from __future__ import unicode_literals
+from __future__ import (absolute_import, division, print_function, unicode_literals)
 
 from unittest import TestCase
 
