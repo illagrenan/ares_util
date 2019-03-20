e2197349902da723974219055c4b88ee9c8ea940
Václav Dohnal <vaclav.dohnal@gmail.com>
2019-03-20 23:16:01 +0100

    modernize tasks

--- tasks.py
+++ tasks.py
@@ -1,15 +1,25 @@
 # -*- encoding: utf-8 -*-
 # ! python3
 
-import os
 import shutil
-import sys
 import warnings
-import webbrowser
 
 from invoke import task
 
 PROJECT_NAME = 'ares_util'
+
+
+@task
+def test(ctx):
+    ctx.run("pytest tests/")
+
+
+@task
+def coverage(ctx):
+    """check code coverage quickly with the default Python"""
+    ctx.run("coverage run --source {PROJECT_NAME} -m pytest".format(PROJECT_NAME=PROJECT_NAME))
+    ctx.run("coverage report -m")
+    ctx.run("coverage html")
 
 
 @task
@@ -23,45 +33,16 @@ def clean(ctx):
 
 
 @task
-def lint(ctx):
-    """check style with flake8"""
-    ctx.run("flake8 {PROJECT_NAME}/ tests/".format(PROJECT_NAME=PROJECT_NAME))
-
-
-@task
-def test(ctx):
-    ctx.run("py.test")
-
-
-@task
-def test_all(ctx):
-    """run tests on every Python version with tox"""
-    ctx.run("tox")
-
-
-@task
 def check(ctx):
     """Check setup"""
     ctx.run("python setup.py --no-user-cfg --verbose check --metadata --restructuredtext --strict")
 
 
 @task
-def coverage(ctx):
-    """check code coverage quickly with the default Python"""
-    ctx.run("coverage run --source {PROJECT_NAME} -m py.test".format(PROJECT_NAME=PROJECT_NAME))
-    ctx.run("coverage report -m")
-    ctx.run("coverage html")
-
-    if not os.getenv('TRAVIS') and (sys.stdout.isatty() and sys.stdin.isatty() and sys.stderr.isatty()):
-        # Running in a real terminal
-        webbrowser.open('file://' + os.path.realpath("htmlcov/index.html"), new=2)
-
-
-@task
 def test_install(ctx):
     """try to install built package"""
     ctx.run("pip uninstall {PROJECT_NAME} --yes".format(PROJECT_NAME=PROJECT_NAME), warn=True)
-    ctx.run("pip install --use-wheel --no-cache-dir --no-index --find-links=file:./dist {PROJECT_NAME}".format(PROJECT_NAME=PROJECT_NAME))
+    ctx.run("pip install --no-cache-dir --no-index --find-links=file:./dist {PROJECT_NAME}".format(PROJECT_NAME=PROJECT_NAME))
     ctx.run("pip uninstall {PROJECT_NAME} --yes".format(PROJECT_NAME=PROJECT_NAME))
 
 
