9a24c31bb1e324ccdd9e2c05f0a1414481d66f5d
Václav Dohnal <vaclav.dohnal@gmail.com>
2019-03-20 22:53:03 +0100

    use https ares URL

--- ares_util/settings.py
+++ ares_util/settings.py
@@ -4,4 +4,4 @@
 from __future__ import (absolute_import, division, print_function, unicode_literals)
 
 COMPANY_ID_LENGTH = 8
-ARES_API_URL = 'http://wwwinfo.mfcr.cz/cgi-bin/ares/darv_bas.cgi'
+ARES_API_URL = 'https://wwwinfo.mfcr.cz/cgi-bin/ares/darv_bas.cgi'
